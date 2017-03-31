d061f1f11b393fbf7aed1829c5d63787f60dcac8
Polad <polad.ibrahim27@gmail.com>
2017-03-31 11:57:03 +0400

    Add files via upload

---
+++ callback/js/callback.js
@@ -0,0 +1,25 @@
+var img = document.getElementById('img');
+var button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+    this.style.border = "5px solid";
+    balacalat(400, 200, 1000, boyut);
+    
+});
+
+function balacalat(w, h, saniye, callback) {
+    var timeout = setTimeout(function() {
+        img.style.width = w;
+        img.style.height = h;
+        img.style.transition = 'all 0.5s ease';
+        callback(600, 350, saniye);
+    }, saniye);
+}
+
+function boyut(w, h, prm_saniye) {
+    var set = setTimeout(function() {
+        img.style.width = w;
+        img.style.height = h;
+        img.style.transition = 'all 0.5s ease';
+    }, prm_saniye);
+}
